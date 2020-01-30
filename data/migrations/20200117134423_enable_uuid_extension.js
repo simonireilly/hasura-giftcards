b556eeaf50d67f254d936f231d01c6e3acaba2b5
Adam Dullenty <adam.dullenty@gmail.com>
2020-01-30 15:14:38 +0000

    Add uuid for auth code

---
+++ data/migrations/20200117134423_enable_uuid_extension.js
@@ -0,0 +1,7 @@
+exports.up = function (knex) {
+  return knex.raw('create extension if not exists "uuid-ossp"')
+}
+
+exports.down = function (knex) {
+  return knex.raw('drop extension if exists "uuid-ossp"')
+}
